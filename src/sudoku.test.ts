f212414e3a175d0d1e7d61b4e205ccdb22371ad7
Mark Cheeseman <mcheesemanau@gmail.com>
2021-06-22 23:29:27 +1000

    Add tests for checking uniqueness
    * also add arbitrary creation for tests

--- src/sudoku.test.ts
+++ src/sudoku.test.ts
@@ -45,10 +45,10 @@ const getColumnData = (sudokuPuzzle: Puzzle, column: number, rowSize: number) =>
 }
 
 const isValidRow = (sudokuPuzzle: Puzzle, row: number): Boolean => 
-  areAllOptionElementsUnique(sudokuPuzzle.slice(row*9, 9));
+  areAllSomesUnique(sudokuPuzzle.slice(row*9, 9));
 
 const isValidColumn = (sudokuPuzzle: Puzzle, column: number): Boolean => 
-  areAllOptionElementsUnique(getColumnData(sudokuPuzzle, column, 9));
+  areAllSomesUnique(getColumnData(sudokuPuzzle, column, 9));
 
 const isValidSquare = (sudokuPuzzle: Puzzle, square: number): Boolean => {
   const startPos = (square%3)* 27 + (square%3 * 3);
@@ -60,26 +60,49 @@ const isValidSquare = (sudokuPuzzle: Puzzle, square: number): Boolean => {
     .concat(sudokuPuzzle.slice(startPos + secondRowShift, 3))
     .concat(sudokuPuzzle.slice(startPos + thirdRowShift, 3));
 
-  return areAllOptionElementsUnique(squareData);
+  return areAllSomesUnique(squareData);
 }
 
 const reducer = <T>(acc: T[], next: Option<T>) => 
   next.map(n => [...acc, n]).getOrElse(()=>acc);
 
-const areAllOptionElementsUnique = (a: Option<number>[]): Boolean => {
+const areAllSomesUnique = (a: Option<number>[]): Boolean => {
   let start : number[] = [];
   const somes = a.reduce(reducer, start);
   return areAllElementsUnique(somes);
 };
 
+const arbArrayWithRepeats: Arbitrary<number[]> = 
+  fc.array(fc.integer(), {minLength: 1})
+  .chain(a => fc.tuple(fc.constant(a), fc.integer({min:a.length+1, max:a.length*2})))
+  .chain(x => {
+    const seedArray = x[0];
+    const arrayLength = x[1];
+    return fc.tuple(
+      fc.constant(seedArray), 
+      fc.array(fc.integer({min:0, max:seedArray.length - 1}), {minLength:arrayLength, maxLength: arrayLength}))
+  })
+  .map(x => {
+    const seedArray = x[0];
+    const positions = x[1];
+    return positions.map(i => seedArray[i])
+  });
+
 const areAllElementsUnique = (a: number[])=>
   (new Set<number>(a).size) === a.length;
 
-// describe('isValidRow', () => {
-//   test('returns true unless there are duplicates', () => {
-//     fc.assert(fc.property(noRepeatArray, (a) => {
-//       console.log(a)
-//     }));
-//   })
-// })
+describe('areAllElementsUnique', () => {
+  test('returns true when there are no duplicates', () => {
+    fc.assert(fc.property(fc.set(fc.integer()), (a) => {
+      expect(areAllElementsUnique(a)).toEqual(true);
+    }));
+  })
+
+  test('returns false when there are duplicates', () => {
+    fc.assert(fc.property(arbArrayWithRepeats, (a) => {
+      console.log(a);
+      expect(areAllElementsUnique(a)).toEqual(false);
+    }));
+  })
+})
 
