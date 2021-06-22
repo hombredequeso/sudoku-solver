fd7419caec2e0cd6187059c3524667bb47d57387
Mark Cheeseman <mcheesemanau@gmail.com>
2021-06-22 21:50:02 +1000

    Basic validation functions added

--- src/sudoku.test.ts
+++ src/sudoku.test.ts
@@ -1,4 +1,5 @@
 import * as fc from 'fast-check';
+import { Arbitrary } from 'fast-check';
 import {Option, option, none} from 'ts-option';
 
 type Place = Option<number>;
@@ -12,8 +13,8 @@ const merge = (puzzle: Puzzle, solutionTree: SolutionTree): Puzzle =>
     p
   );
 
-const intOption: Arbitrary<Option<int>> = fc.integer().map(i => option(i));
-const intOption2: Arbitrary<Option<int>> = fc.boolean().chain(b => b ? intOption : fc.constant(none));
+const intOption: Arbitrary<Option<number>> = fc.integer().map(i => option(i));
+const intOption2: Arbitrary<Option<number>> = fc.boolean().chain(b => b ? intOption : fc.constant(none));
 
 describe('merge', () => {
   test('merges puzzle and solutionTree', () => {
@@ -33,15 +34,52 @@ describe('merge', () => {
   });
 });
 
-const noRepeatArray: Arbitrary<int[]> = fc.array(fc.nat()).map(a => a.fold)
+const getColumnData = (sudokuPuzzle: Puzzle, column: number, rowSize: number) => {
+  let columnData = [];
+  let pos = column;
+  while (pos < sudokuPuzzle.length) {
+    columnData.push(sudokuPuzzle[pos])
+    pos = pos + rowSize;
+  }
+  return columnData;
+}
 
-// const isValidRow(sudokuPuzzle, row): Boolean => {}
+const isValidRow = (sudokuPuzzle: Puzzle, row: number): Boolean => 
+  areAllOptionElementsUnique(sudokuPuzzle.slice(row*9, 9));
 
-describe('isValidRow', () => {
-  test('returns true unless there are duplicates', () => {
-    fc.assert(fc.property(fc.array(fc.nat()), (a) => {
-      console.log(a)
-    }));
-  })
-})
+const isValidColumn = (sudokuPuzzle: Puzzle, column: number): Boolean => 
+  areAllOptionElementsUnique(getColumnData(sudokuPuzzle, column, 9));
+
+const isValidSquare = (sudokuPuzzle: Puzzle, square: number): Boolean => {
+  const startPos = (square%3)* 27 + (square%3 * 3);
+  const secondRowShift = 9;
+  const thirdRowShift = 18;
+
+  const squareData = 
+    sudokuPuzzle.slice(startPos, 3)
+    .concat(sudokuPuzzle.slice(startPos + secondRowShift, 3))
+    .concat(sudokuPuzzle.slice(startPos + thirdRowShift, 3));
+
+  return areAllOptionElementsUnique(squareData);
+}
+
+const reducer = <T>(acc: T[], next: Option<T>) => 
+  next.map(n => [...acc, n]).getOrElse(()=>acc);
+
+const areAllOptionElementsUnique = (a: Option<number>[]): Boolean => {
+  let start : number[] = [];
+  const somes = a.reduce(reducer, start);
+  return areAllElementsUnique(somes);
+};
+
+const areAllElementsUnique = (a: number[])=>
+  (new Set<number>(a).size) === a.length;
+
+// describe('isValidRow', () => {
+//   test('returns true unless there are duplicates', () => {
+//     fc.assert(fc.property(noRepeatArray, (a) => {
+//       console.log(a)
+//     }));
+//   })
+// })
 
